4708efd35ce5aef056da70e7d461a287c833a7d4
Bernard Nixon <39972453+nardosius@users.noreply.github.com>
2023-10-23 09:19:33 -0500

    Add files via upload

---
+++ Interfaces/Animal.java
@@ -0,0 +1,23 @@
+interface FlightEnabled {
+//in interfaces, methods have no bodies ND END WITH "();"
+    void takeoff();
+
+    void land();
+
+    void fly();
+
+
+}
+
+interface Trackable {
+
+    void track();
+
+}
+
+public abstract class Animal {
+
+    public void move() {
+
+    }
+}
