08d988d143c629b8dcd6452602f4f53854d8e95e
nardosius <nixonbii3@gmail.com>
2023-11-17 14:52:45 -0600

    code mods 111723

---
+++ DataStructures/src/ListIterator.java
@@ -0,0 +1,45 @@
+import java.util.LinkedList;
+
+record Place(String name, int distance){
+    @Override
+    public String toString(){
+        return String.format("%s (%d)", name, distance);
+    }
+
+}
+public class ListIterator {
+
+    public static void addPlace(LinkedList<Place> list, Place place){
+
+        if (list.contains(place)){
+            System.out.println("Found duplicate entry: " + place);
+            return;
+        }
+        for (Place p : list){
+            if(p.name().equalsIgnoreCase(place.name())){
+                System.out.println("Found duplicate entry: " + place);
+                return;
+            }
+        }
+        int matchedIndex = 0;
+        for (var listPlace : list) {
+            if (place.distance() < listPlace.distance()) {
+                list.add(matchedIndex, place);
+                return;
+            }
+            matchedIndex++;
+        }
+        list.add(place);
+    }
+
+    public static void printMenu() {
+        String textBlock = """
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackward
+                (L)ist Places
+                (M)enu
+                (Q)uit""";
+        System.out.print(textBlock + " ");
+    }
+}
--- DataStructures/src/Main.java
+++ DataStructures/src/Main.java
@@ -7,8 +7,6 @@ record groceryItems(String name, String type, int count){
     }
 }
 
-
-
 public class Main {
 
     public static void main(String[] args) {
@@ -38,8 +36,84 @@ public class Main {
         nextList.sort(Comparator.naturalOrder());
         System.out.println(nextList.toString());*/
 
-        ArListChallenge arListChallenge = new ArListChallenge();
-        arListChallenge.menu();
+        /*ArListChallenge arListChallenge = new ArListChallenge();
+        arListChallenge.menu();*/
 
+        LinkedList<Place> placesToVisit = new LinkedList<>();
+        Place adelaide = new Place("Adelaide", 1374);
+        Place brisbane = new Place("Brisbane", 917);
+        Place perth = new Place("Perth", 3923);
+        Place aliceSprings = new Place("Alice Springs", 2771);
+        Place darwin = new Place("Darwin", 3972);
+        Place melbourne = new Place("Melbourne", 877);
+        ListIterator.addPlace(placesToVisit, adelaide);
+        ListIterator.addPlace(placesToVisit, brisbane);
+        ListIterator.addPlace(placesToVisit, perth);
+        ListIterator.addPlace(placesToVisit, aliceSprings);
+        ListIterator.addPlace(placesToVisit, darwin);
+        ListIterator.addPlace(placesToVisit, melbourne);
+
+        placesToVisit.addFirst(new Place("Sydney", 0));
+        System.out.println(placesToVisit);
+        StringBuilder s = new StringBuilder();
+                for (Place p : placesToVisit) {
+            s.append(p.name() + " ").append(p.distance() + ", ");
+        }
+        System.out.println(s);
+                var iterator = placesToVisit.listIterator();
+                Scanner scanner = new Scanner(System.in);
+                boolean quitLoop = false;
+                boolean forward = true;
+
+                ListIterator.printMenu();
+        System.out.println();
+                while (!quitLoop) {
+                    if (!iterator.hasPrevious()) {
+                        System.out.println("Originating : " + iterator.next());
+                        forward = true;
+                    }
+                    if (!iterator.hasNext()) {
+                        System.out.println("Final : " + iterator.previous());
+                        forward = false;
+                    }
+                    System.out.println("Enter Value: ");
+                    String menuItem = scanner.nextLine().toUpperCase().substring(0, 1);
+
+                    switch (menuItem) {
+                        case "F":
+                            System.out.println("User wants to go forward");
+                            if (!forward) {//reversing
+                                forward = true;
+                                if (iterator.hasNext()) {
+                                    iterator.next();
+                                }
+                            }
+                            if (iterator.hasNext()) {
+                                System.out.println(iterator.next());
+                            }
+                            break;
+                        case "B":
+                            System.out.println("User wants to go backwards");
+                            if (forward) {//reversing
+                                forward = false;
+                                if (iterator.hasPrevious()) {
+                                    iterator.previous();
+                                }
+                            }
+                            if (iterator.hasPrevious()) {
+                                System.out.println(iterator.previous());
+                            }
+                            break;
+                        case "M":
+                            ListIterator.printMenu();
+                            break;
+                        case "L":
+                            System.out.println(placesToVisit);
+                            break;
+                        default:
+                            quitLoop = true;
+                            break;
+                    }
+                }
     }
 }
