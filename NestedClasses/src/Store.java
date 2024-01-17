de440c4ade49555b437ec25b9bf8f23f22ea6132
nardosius <nixonbii3@gmail.com>
2024-01-16 18:28:46 -0600

    anonymous classes

--- NestedClasses/src/Store.java
+++ NestedClasses/src/Store.java
@@ -1,3 +1,6 @@
+import java.util.Comparator;
+import java.util.List;
+
 public class Store {
 
     /*This class used a main method in the Udemy course. My habit is to have only one
@@ -6,6 +9,13 @@ public class Store {
     * public staic void main (String[] args) {
     * Meal regularMeal = new Meal();\
     * System.out.println(regularMeal);
-    * */
+    *  the following is a generic comparator method:*/
+    public static <T> void  sortIt(List<T> list, Comparator<? super T> comparator) {
+        System.out.println("Sorting with Comparator: " + comparator.toString());
+        list.sort(comparator);
+        for (var employee : list) {
+            System.out.println(employee);
+        }
+    }
 
 }
